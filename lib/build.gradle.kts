20acf0f84c7bf607488e91c96d14cab73497562c
fish895623 <dan990429@gmail.com>
2021-12-13 19:33:28 +0000

    fix

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,40 +1,40 @@
 plugins {
-    id("java-library")
-    id("maven-publish")
+  `java-library`
+  `maven-publish`
 }
 
 group = "com.github.fish895623"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
 
-    // Must include
-    api("org.apache.commons:commons-math3:3.6.1")
+  // Must include
+  api("org.apache.commons:commons-math3:3.6.1")
 
-    implementation("com.google.guava:guava:30.1.1-jre")
+  implementation("com.google.guava:guava:30.1.1-jre")
 }
 
 publishing {
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/fish895623/java-utils")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System
-                    .getenv("GITHUB_USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System
-                    .getenv("GITHUB_TOKEN")
-            }
-        }
+  repositories {
+    maven {
+      url = uri("https://maven.pkg.github.com/fish895623/java-utils")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System
+          .getenv("GITHUB_USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System
+          .getenv("GITHUB_TOKEN")
+      }
     }
-    publications {
-        register<MavenPublication>("gpr") {
-            artifactId = "utils"
-            from(components["java"])
-        }
+  }
+  publications {
+    create<MavenPublication>("main") {
+      artifactId = "utils"
+      from(components["java"])
     }
+  }
 }
