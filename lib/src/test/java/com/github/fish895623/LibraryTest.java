a6c2ef0a66acf45c26db45c4dd4653b1bab86c97
fish895623 <dan990429@gmail.com>
2021-12-13 22:57:24 +0900

    indent

--- lib/src/test/java/com/github/fish895623/LibraryTest.java
+++ lib/src/test/java/com/github/fish895623/LibraryTest.java
@@ -4,11 +4,14 @@
 package com.github.fish895623;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
-        Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
-    }
+  @Test
+  void someLibraryMethodReturnsTrue() {
+    Library classUnderTest = new Library();
+    assertTrue(classUnderTest.someLibraryMethod(), "should be " +
+      "true");
+  }
 }
